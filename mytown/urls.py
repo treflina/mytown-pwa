36c2d5795cdd95424a96258b2c27ef3980a5cb1a
treflina <treflina@wp.pl>
2024-01-17 21:11:27 +0100

    manifest.json moved

--- mytown/urls.py
+++ mytown/urls.py
@@ -8,7 +8,7 @@ from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
     path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
-    # path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json'), name='manifest.json'),
+    path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json'), name='manifest.json'),
     path("admin/", admin.site.urls),
     path("", include("garbage.urls")),
     path("", include("notifications.urls")),
