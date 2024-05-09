c0f559de97011c4d330e700f876918fd088a9895
treflina <treflina@wp.pl>
2024-05-09 19:41:23 +0200

    errors info in send_notifications

--- notifications/management/commands/garbage_notifications.py
+++ notifications/management/commands/garbage_notifications.py
@@ -1,11 +1,16 @@
 import json
 import logging
-from django.utils import timezone
 from datetime import timedelta
+
+from django.conf import settings
+from django.core.mail import send_mail
+from django.utils import timezone
 from django.core.management.base import BaseCommand, CommandError
+
 from webpush import send_group_notification
 from webpush.models import Group
 from garbage.models import GarbageCollection, Region
+
 
 logging.root.handlers = []
 logging.basicConfig(
@@ -38,7 +43,18 @@ class Command(BaseCommand):
                     "url": f"/odpady/regions/{region.id}",
                 }
                 if Group.objects.filter(name=region.id):
-                    send_group_notification(
-                        group_name=region.id, payload=payload, ttl=1000
-                    )
-                    logging.info(f"{region.name}: {col.garbage_type.name}")
+                    try:
+                        send_group_notification(
+                            group_name=region.id, payload=payload, ttl=1000
+                        )
+                        logging.info(f"{region.name}: {col.garbage_type.name}")
+                    except Exception as e:
+                        send_mail(
+                            "eTurawa",
+                            f"{e}",
+                            settings.EMAIL_HOST_USER,
+                            [settings.EMAIL_HOST_USER],
+                            fail_silently=False,
+                        )
+                        logging.error(f"Błąd przy próbie wysłania powiadomień: {e}")
+
